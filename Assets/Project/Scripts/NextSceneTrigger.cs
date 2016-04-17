d73f43712338a3eb2c0714ae686a5dee882554d6
rampage0131 <jake.docker0131@gmail.com>
2016-04-17 18:18:43 -0400

    Finished implementing the end gate.

---
+++ Assets/Project/Scripts/NextSceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using OmiyaGames;
+[RequireComponent(typeof(BoxCollider))]
+public class NextSceneTrigger : MonoBehaviour
+{
+    bool isTriggered = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if((isTriggered == false) && (other.CompareTag("Player") == true))
+        {
+            Singleton.Get<SceneTransitionManager>().LoadNextLevel();
+            isTriggered = true;
+        }
+    }
+}
