56cd4f32b09b2974dbc765bb7ebcc0b493f121de
rampage0131 <jake.docker0131@gmail.com>
2016-04-21 01:06:26 -0400

    Updating the WebLocationChecker to provide it's current state of evaluating the host names. Also, now using C#'s URI library to decipher URL. This allows giving an option to force redirecting the player, or to play nice and check the WebLocationChecker's state itself before deciding what to do.

--- Assets/Unify Community/Scripts/WebLocationChecker.cs
+++ Assets/Unify Community/Scripts/WebLocationChecker.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
+using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
-using System;
 
 namespace OmiyaGames
 {
@@ -27,6 +27,15 @@ namespace OmiyaGames
     /// </summary>
     public class WebLocationChecker : ISingletonScript
     {
+        public enum State
+        {
+            NotUsed = -1,
+            InProgress,
+            EncounteredError,
+            DomainMatched,
+            DomainDidntMatch
+        }
+
         ///<summary>
         /// If it is a webplayer, then the domain must contain any
         /// one or more of these strings, or it will be redirected.
@@ -34,12 +43,6 @@ namespace OmiyaGames
         ///</summary>
         [SerializeField]
         List<string> domainMustContain;
-        ///<summary>
-        /// This is where to redirect the webplayer page if none of
-        /// the strings in domainMustContain are found.
-        ///</summary>
-        [SerializeField]
-        string redirectURL;
 
         ///<summary>
         /// (optional) or fetch the domain list from this URL
@@ -58,14 +61,69 @@ namespace OmiyaGames
         [SerializeField]
         GameObject[] waitObjects;
 
+        ///<summary>
+        /// If true, the game will force the webplayer to redirect to
+        /// the URL below
+        ///</summary>
+        [Header("Redirect Options")]
+        [SerializeField]
+        bool forceRedirectIfDomainDoesntMatch = true;
+        ///<summary>
+        /// This is where to redirect the webplayer page if none of
+        /// the strings in domainMustContain are found.
+        ///</summary>
+        [SerializeField]
+        string redirectURL;
+
+        State currentState = State.NotUsed;
+        bool downloadedRemoteDomainList = false;
+
+        #region Properties
+        public State CurrentState
+        {
+            get
+            {
+                return currentState;
+            }
+            private set
+            {
+                currentState = value;
+            }
+        }
+
+        public bool IsRemoteDomainListSuccessfullyDownloaded
+        {
+            get
+            {
+                return downloadedRemoteDomainList;
+            }
+            private set
+            {
+                downloadedRemoteDomainList = value;
+            }
+        }
+        #endregion
+
         public override void SingletonAwake(Singleton instance)
         {
-            // Shoot a coroutine
 #if (UNITY_WEBPLAYER || UNITY_WEBGL)
-            if (domainMustContain.Count > 0)
+#if !UNITY_EDITOR
+            // Shoot a coroutine if not in editor, and making Webplayer or WebGL
+            StartCoroutine(CheckDomainList());
+#else
+            // Do a little bit of debugging
+            StringBuilder buf = new StringBuilder();
+            if (string.IsNullOrEmpty(remoteDomainListUrl) == false)
             {
-                StartCoroutine(CheckDomainList());
+                // Print remote domain list URL
+                Debug.Log("Example URL to grab remote Domain List will look like:\n" + GenerateRemoteDomainList(buf));
             }
+            if (string.IsNullOrEmpty(redirectURL) == false)
+            {
+                // Print redirect javascript
+                Debug.Log("Redirect javascript will look like:\n" + GenerateRedirect(buf));
+            }
+#endif
 #endif
         }
 
@@ -74,85 +132,145 @@ namespace OmiyaGames
             // Do nothing
         }
 
+        public void ForceRedirect()
+        {
+            ForceRedirect(new StringBuilder());
+        }
+
+        #region Helper Methods
 #if (UNITY_WEBPLAYER || UNITY_WEBGL)
         IEnumerator CheckDomainList()
         {
-                // Deactivate any objects
-                int index = 0;
-                for (index = 0; index < waitObjects.Length; ++index)
+            // Update state
+            CurrentState = State.InProgress;
+
+            // Deactivate any objects
+            int index = 0;
+            for (index = 0; index < waitObjects.Length; ++index)
+            {
+                waitObjects[index].SetActive(false);
+            }
+
+            // Grab a domain list remotely
+            StringBuilder buf = new StringBuilder();
+            if (string.IsNullOrEmpty(remoteDomainListUrl) == false)
+            {
+                // Grab remote domain list
+                WWW www = new WWW(GenerateRemoteDomainList(buf));
+                yield return www;
+
+                // Check if there were any errors
+                if (string.IsNullOrEmpty(www.error) == true)
                 {
-                    waitObjects[index].SetActive(false);
+                    // If none, split the text file we've downloaded, and add it to the list
+                    domainMustContain.AddRange(www.text.Split(splitRemoteDomainListUrlBy));
+                    IsRemoteDomainListSuccessfullyDownloaded = true;
                 }
+            }
 
-                // Grab the domains List from 
-                StringBuilder buf = new StringBuilder();
-                if (string.IsNullOrEmpty(remoteDomainListUrl) == false)
+            // Make sure there's at least one domain we need to check
+            if (domainMustContain.Count > 0)
+            {
+                // parse the page's address
+                bool isErrorEncountered = false;
+                if (IsHostMatchingListedDomain(domainMustContain, out isErrorEncountered) == true)
                 {
-                    // Grab remote domain list
-                    buf.Length = 0;
-                    AppendRemoteDomainList(buf);
-
-                    WWW www = new WWW(buf.ToString());
-                    yield return www;
-
-                    // Check if there were any errors
-                    if (string.IsNullOrEmpty(www.error) == true)
+                    // Update state
+                    CurrentState = State.DomainMatched;
+                }
+                else
+                {
+                    // Update state
+                    if(isErrorEncountered == true)
                     {
-                        // If none, split the text file we've downloaded, and add it to the list
-                        domainMustContain.AddRange(www.text.Split(splitRemoteDomainListUrlBy));
+                        CurrentState = State.EncounteredError;
+                    }
+                    else
+                    {
+                        CurrentState = State.DomainDidntMatch;
+                    }
+
+                    // Check if we should force redirecting the player
+                    if (forceRedirectIfDomainDoesntMatch == true)
+                    {
+                        ForceRedirect(buf);
                     }
                 }
+            }
+            else
+            {
+                // Update state
+                CurrentState = State.NotUsed;
+            }
 
-                // Generate javascript code
-                buf.Length = 0;
-                AppendDomainCheckingConditionals(buf);
-                AppendRedirect(buf);
-
-                // Evaluate the javascript
-#if !UNITY_EDITOR
-            Application.ExternalEval(buf.ToString());
-#else
-                buf.Insert(0, "Output javascript in WebLocationCheck.cs:\n");
-                Debug.Log(buf.ToString());
-#endif
-
-                // Reactivate any objects
-                for (index = 0; index < waitObjects.Length; ++index)
-                {
-                    waitObjects[index].SetActive(true);
-                }
+            // Reactivate any objects
+            for (index = 0; index < waitObjects.Length; ++index)
+            {
+                waitObjects[index].SetActive(true);
+            }
         }
 
-        void AppendRemoteDomainList(StringBuilder buf)
+        string GenerateRemoteDomainList(StringBuilder buf)
         {
+            buf.Length = 0;
             buf.Append(remoteDomainListUrl);
             buf.Append("?r=");
             buf.Append(UnityEngine.Random.value);
+            return buf.ToString();
         }
 
-        void AppendDomainCheckingConditionals(StringBuilder buf)
+        bool IsHostMatchingListedDomain(List<string> domainList, out bool encounteredError)
         {
-            buf.AppendLine("if((document.location.protocol != 'file:') &&");
-            for (int index = 0; index < domainMustContain.Count; index++)
+            Uri uri;
+            bool isTheCorrectHost = false;
+
+            // Evaluate the URL
+            encounteredError = true;
+            if (Uri.TryCreate(Application.absoluteURL, UriKind.Absolute, out uri) == true)
             {
-                if (index > 0)
-                {
-                    buf.AppendLine(" &&");
-                }
+                // Indicate there were no errors
+                encounteredError = false;
 
-                buf.Append("  (document.location.host.indexOf('");
-                buf.Append(domainMustContain[index]);
-                buf.Append("') == -1)");
+                // Check if the scheme isn't file (i.e. local file run on computer)
+                if (uri.Scheme != "file")
+                {
+                    // Make sure host matches any one of the domains
+                    for (int index = 0; index < domainList.Count; ++index)
+                    {
+                        if (uri.Host == domainList[index])
+                        {
+                            isTheCorrectHost = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    // If this is a file run by a local computer, indicate domain matched
+                    isTheCorrectHost = true;
+                }
             }
-            buf.AppendLine(")");
+            return isTheCorrectHost;
         }
 
-        void AppendRedirect(StringBuilder buf)
+        void ForceRedirect(StringBuilder buf)
         {
-            buf.Append("{ window.top.location='");
+            if (string.IsNullOrEmpty(redirectURL) == false)
+            {
+                // Evaluate the javascript
+                Application.ExternalEval(GenerateRedirect(buf));
+            }
+        }
+
+        string GenerateRedirect(StringBuilder buf)
+        {
+            buf.Length = 0;
+            buf.Append("window.top.location='");
             buf.Append(redirectURL);
-            buf.Append("'; }");
+            buf.Append("';");
+            return buf.ToString();
         }
 #endif
+        #endregion
     }
 }
